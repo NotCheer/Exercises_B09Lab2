20ac574a5a0a24df5e2c3f0d5912c3597fc73934
NotCheer <nut.li@mail.utoronto.ca>
2024-02-28 15:46:35 -0500

    modified

--- MyFDViewer.c
+++ MyFDViewer.c
@@ -24,17 +24,49 @@ typedef struct Arguments {
     int threshold;
 } arguments;
 
-typedef struct FDinfo {
-    int PID;
+typedef struct FDNode {
     int FD;
-    char filename[MAX_STR_LEN];
+
+    struct FDNode* next;
+} fdNode;
+
+typedef struct ProcessInfoNode {
+    int PID;
     int Inode;
+    char filename[MAX_STR_LEN];
+    fdNode* FD;
 
-    struct FDinfo* next;
-} fdinfo;
+    struct ProcessInfoNode* next;
+} processInfoNode;
 
-fdinfo* creatNode(int PID, int FD, int Inode, char* filename) {
-    fdinfo* newNode = (fdinfo*)malloc(sizeof(fdinfo));
+fdNode* createFDNode(int FD) {
+    fdNode* newNode = (fdNode*)malloc(sizeof(fdNode));
+    newNode->FD = FD;
+    newNode->next = NULL;
+    return newNode;
+}
+
+fdNode* insertFDNode(fdNode* root, fdNode* node) {
+    if(root == NULL) return node;
+    root->next = insertFDNode(root->next, node);
+}
+
+int isProcessOwner(char* path) {
+    struct stat statbuf;
+    if (stat(path, &statbuf) == -1) {
+        perror("error getting stat.");
+        return -2;
+    }
+
+    uid_t uid = getuid();
+    gid_t gid = getgid();
+
+    if(statbuf.st_uid == uid && statbuf.st_gid == gid) return 0;
+    return -1;
+}
+
+processInfoNode* creatProcessNode(int PID, int Inode, fdNode* FD, char* filename) {
+    processInfoNode* newNode = (processInfoNode*)malloc(sizeof(processInfoNode));
     newNode->PID = PID;
     newNode->FD = FD;
     newNode->Inode = Inode;
@@ -43,24 +75,82 @@ fdinfo* creatNode(int PID, int FD, int Inode, char* filename) {
     return newNode;
 }
 
-fdinfo* insertNode(fdinfo* root, fdinfo* node) {
+processInfoNode* insertProcessNode(processInfoNode* root, processInfoNode* node) {
     if(root == NULL) return node;
-    node->next = insertNode(root->next, node);
+    node->next = insertProcessNode(root->next, node);
     return root;
 }
 
-fdinfo* retriveFDInfo(struct dirent* dir) {
+// get info of a process given by the fd at `dir`, store it in linked list rooted at root
+processInfoNode* retriveFDInfo(processInfoNode* root, struct dirent* dir) {
+    char path[MAX_STR_LEN];
+    char buffer[MAX_STR_LEN];
+    char name[MAX_STR_LEN];
+    long inode;
+    int pid = -1;
+    fdNode* FD=NULL;
+    sprintf(path, "/proc/%s/fd", dir->d_name);
+    if(isProcessOwner(path) != 0) return root;
+    
+    sprintf(path, "/proc/%s/status", dir->d_name);
+    FILE* fp = fopen(path, "r");
 
+    if(fp == NULL) {
+        perror("Error opening /proc/pid/ststus\n");
+        return root;
+    }
+
+    //get inode
+    inode = dir->d_ino;
+
+    //get name
+    fgets(buffer, MAX_STR_LEN, fp);
+    strcpy(name, &buffer[6]);
+    while(!feof(fp)) {
+        //get pid
+        if(strncmp("Pid", buffer, 3) == 0){
+            pid = strtol(&buffer[5], NULL, 10); 
+            break;
+        }
+        fgets(buffer, MAX_STR_LEN, fp);
+    }
+    fclose(fp);
+
+    //////////////////////////////////////////
+    // read FD info
+    struct stat statbuf;
+    sprintf(path, "/proc/%s/fp", dir->d_name);
+    setuid(getuid());
+    DIR* proc = opendir(path);
+
+    if(proc == NULL) return root;
+
+    struct dirent* fdDir;
+    
+    while((fdDir = readdir(proc)) != NULL) {
+        long fd = strtol(fdDir->d_name, NULL, 10);
+        fdNode* f = createFDNode(fd);
+        FD = insertFDNode(FD, f);
+        printf("got fd: %ld\n", fd);
+    }
+    closedir(proc);
+
+    processInfoNode* newNode = creatProcessNode(pid, inode, FD, name);
+    root = insertProcessNode(root, newNode);
+    return root;
 }
 
-// Open the `/proc` directory and initialize our FDinfo linked list
-int initFDList(fdinfo** root) {
+// Open the `/proc` directory and initialize our ProcessInfoNode linked list
+int initFDList(processInfoNode* root) {
     DIR* proc = opendir("/proc");
     struct dirent* dir;
     int len = 0;
     
     while((dir = readdir(proc)) != NULL) {
-        printf("%ld, %s\n",dir->d_ino, dir->d_name);
+        //printf("%ld, %s\n",dir->d_ino, dir->d_name);
+        root = retriveFDInfo(root, dir);
+        if(root) printf("%d\n",(root)->FD->FD);
+        len++;
     }
 
     closedir(proc);
@@ -68,5 +158,8 @@ int initFDList(fdinfo** root) {
 }
 
 int main(int argc, char* argv[]) {
-    initFDList(NULL);
+    setuid(getuid());
+    
+    processInfoNode* root = NULL;
+    initFDList(root);
 }
