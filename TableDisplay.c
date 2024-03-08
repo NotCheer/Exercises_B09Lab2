f3efb10e14b0bc8c5cf10fe5fcfd95321ceee98a
NotCheer <nut.li@mail.utoronto.ca>
2024-03-08 10:33:21 -0500

    add a module

--- TableDisplay.c
+++ TableDisplay.c
@@ -1,9 +1,14 @@
+#include "ProcessStruct.h"
+
 #include<stdio.h>
 #include<stdlib.h>
 
-#include "ProcessStruct.h"
-
-void assemble()
+void assembleHead(arguments* args)
 {
-    
+
+}
+
+void assemble(arguments* args, processInfoNode* head)
+{
+    assembleHead(args);
 }
--- TableDisplay.h
+++ TableDisplay.h
@@ -1,6 +1,12 @@
 #ifndef TABLE_DISPLAY_H
 #define TABLE_DISPLAY_H
 
-void assemble();
+#include "ProcessStruct.h"  
 
-#endif TABLE_DISPLAY_H
+#include<stdio.h>
+#include<stdlib.h>
+
+void assemble(arguments* args, processInfoNode* head);
+void assembleHead(arguments* args);
+
+#endif
